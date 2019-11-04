f4ecd2d8229b6fc16690b6ca4468f3352f7092b7
yudeunagi <yudeunagi@gmail.com>
2019-11-04 09:49:15 +0900

    ・ポート番号指定がブラウザ側は3000固定になってたのを修正 ・if-modified-sinceをリクエストヘッダに追加

--- src/main/readBBS/Read5ch.js
+++ src/main/readBBS/Read5ch.js
@@ -3,13 +3,17 @@
 */
 const request = require('request-promise'); //httpリクエスト
 const iconv = require('iconv-lite'); // 文字コード変換用パッケージ
+//ステータスコード304 _NotModified
+const NOT_MODIFIED = '304';
 
+// 最終取得スレッド
+var lastThreadUrl = '';
 // 最終レス番号
 var lastResNumber = 0;
-// 最終バイト数
-var lastByte = 0;
 //最終更新日時
 var lastModified = null;
+// 最終バイト数
+var lastByte = 0;
 
 /**
 * コンストラクタ
@@ -28,6 +32,17 @@ var ReadSitaraba = function()
 * @param String // resNum レス番号
 */
 ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
+
+
+  //板や最終日レス番号がかわったら最初からとり直す(lastmodifiと rangeのリセット)
+  if(threadUrl != lastThreadUrl
+  || resNum < lastResNumber ){
+    lastThreadUrl = threadUrl;
+    lastModified = null;
+    lastByte = 0;
+  }else {
+    console.log('noresete');
+  }
 
   //リクエストURL作成 下記みたいな感じで変換する
   //https://bbs.jpnkn.com/test/read.cgi/yudeunagi/1572734724/
@@ -52,23 +67,56 @@ ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
     url: requestUrl,
     method: 'GET',
     encoding: null, // ここでnull指定しないとなんかうまくいかない
-    "If-Modified-Since": lastModified,
-    "Range": range
+    "resolveWithFullResponse": true,
+    headers: {
+      "if-modified-since": lastModified,
+      range: range,
+    }
   }
+  console.log(options);
 
   var responseJson;
   //掲示板へのリクエスト実行
   console.log('[Read5ch.js]5ch系BBSレス取得API呼び出し開始');
   await request(options)
-  .then(body =>{
-    console.log('[Read5ch.js]5ch系BBSレス取得API呼び出し成功');
+  .then(response =>{
+    var statusCode = response.statusCode;
+    console.log('[Read5ch.js]5ch系BBSレス取得API呼び出し完了、statusCode=' + statusCode);
+
+    // 取得バイト数表示
+    console.log('[Read5ch.read]レスポンスヘッダ=');
+    var headers = response.headers;
+    console.log(headers);
+    //LastModifiedとRange更新処理
+    if(headers['last-modified'] != null){
+      lastModified = headers['last-modified'];
+      console.log(lastModified);
+    }
+//    if(headers['content-length'] != null){
+//      console.log(headers['content-length']);
+//      lastByte = lastByte + parseInt(headers['content-length']);
+//      console.log('range=' + lastByte);
+//    }
+
+    //gzipで取得出来たら解凍処理も入れる
+
     //したらばAPIの文字コードはEUC-JPなのでUTF-8に変換する
-    var str = iconv.decode(Buffer.from(body), 'Shift_JIS');
+    var str = iconv.decode(Buffer.from(response.body), 'Shift_JIS');
     // レスポンスオブジェクト作成
     responseJson = purseNewResponse(str, resNum);
+
+    // ヘッダからLast-Modified と content-length数をとる
+
+  })
+  .catch(error =>{
+    console.log('[Read5ch.js]5ch系BBSレス取得APIリクエストエラー、message=' + error.message);
+    var rsArray = new Array();
+    console.log(rsArray.length);
+    responseJson = rsArray;
   });
   return responseJson;
 }
+
 /**
 *取得したレスポンス（複数）のパース
 *戻りとしてパースしたjsonオブジェクトの配列を返す
