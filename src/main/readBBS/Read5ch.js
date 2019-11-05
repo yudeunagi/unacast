cb19d97a7dda7673f13d7fa3acdc75486f077371
yudeunagi <yudeunagi@gmail.com>
2019-11-06 01:52:11 +0900

    差分取得処理実装

--- src/main/readBBS/Read5ch.js
+++ src/main/readBBS/Read5ch.js
@@ -5,6 +5,7 @@ const request = require('request-promise'); //httpリクエスト
 const iconv = require('iconv-lite'); // 文字コード変換用パッケージ
 //ステータスコード304 _NotModified
 const NOT_MODIFIED = '304';
+const RANGE_NOT_SATISFIABLE = '416';
 
 // 最終取得スレッド
 var lastThreadUrl = '';
@@ -36,10 +37,12 @@ ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
 
   //板や最終日レス番号がかわったら最初からとり直す(lastmodifiと rangeのリセット)
   if(threadUrl != lastThreadUrl
-  || resNum < lastResNumber ){
+  || parseInt(resNum) < parseInt(lastResNumber)
+  || resNum == ''){
     lastThreadUrl = threadUrl;
     lastModified = null;
     lastByte = 0;
+    console.log('[Read5ch.js]resete!!!!!!!!!!!!!!!!');
   }else {
     console.log('noresete');
   }
@@ -61,7 +64,7 @@ ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
       490からそれ以降の分をあるだけ
   */
 
-  var range = lastByte + '-';
+  var range = lastByte;
   //リクエストオプションの設定
   var options = {
     url: requestUrl,
@@ -70,6 +73,8 @@ ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
     "resolveWithFullResponse": true,
     headers: {
       "if-modified-since": lastModified,
+      range: 'bytes=' + range + '-',
+//      range: 'bytes=100000-',
     }
   }
   console.log(options);
@@ -82,36 +87,45 @@ ReadSitaraba.prototype.read = async function(threadUrl, resNum, res, next){
     var statusCode = response.statusCode;
     console.log('[Read5ch.js]5ch系BBSレス取得API呼び出し完了、statusCode=' + statusCode);
 
-    // 取得バイト数表示
+    // レスポンスヘッダ表示
     console.log('[Read5ch.read]レスポンスヘッダ=');
     var headers = response.headers;
     console.log(headers);
     //LastModifiedとRange更新処理
     if(headers['last-modified'] != null){
       lastModified = headers['last-modified'];
-      console.log(lastModified);
+      console.log('[Read5ch.read]lastModified=' + lastModified);
     }
-//    if(headers['content-length'] != null){
-//      console.log(headers['content-length']);
-//      lastByte = lastByte + parseInt(headers['content-length']);
-//      console.log('range=' + lastByte);
-//    }
-
     //gzipで取得出来たら解凍処理も入れる
 
     //したらばAPIの文字コードはEUC-JPなのでUTF-8に変換する
     var str = iconv.decode(Buffer.from(response.body), 'Shift_JIS');
-    // レスポンスオブジェクト作成
-    responseJson = purseNewResponse(str, resNum);
+    // レスポンスオブジェクト作成、content-rangeがある場合とない場合で処理を分ける
+    if(headers['content-range'] == null || lastByte == 0){
+      console.log('[Read5ch.read]content-range=' +  headers['content-range']);
+      responseJson = purseNewResponse(str, resNum);
+    }else{
+      responseJson = purseDiffResponse(str, resNum);
+    }
 
-    // ヘッダからLast-Modified と content-length数をとる
+    // 取得バイト数表示
+    if(headers['content-length'] != null && responseJson.length > 0){
+        lastByte = lastByte + parseInt(headers['content-length'] - 1);
+        console.log('[Read5ch.read]lastByte=' + lastByte);
+    }
+
 
   })
   .catch(error =>{
-    console.log('[Read5ch.js]5ch系BBSレス取得APIリクエストエラー、message=' + error.message);
     var rsArray = new Array();
-    console.log(rsArray.length);
     responseJson = rsArray;
+    if(error.status == NOT_MODIFIED){
+      console.log('[Read5ch.js]5ch系BBSレス取得APIリクエストエラー、NOT_MODIFIED');
+    } else if (error.status == RANGE_NOT_SATISFIABLE){
+      console.log('[Read5ch.js]5ch系BBSレス取得APIリクエストエラー、RANGE_NOT_SATISFIABLE');
+    } else {
+      console.log('[Read5ch.js]5ch系BBSレス取得APIリクエストエラー、message=' + error.message);
+    }
   });
   return responseJson;
 }
@@ -133,11 +147,10 @@ function purseNewResponse(res ,resNum){
   // 新着なしなら戻る。
   if(resArray.length == 0){
     return result;
-  } else {
-    // 配列の最後に空の要素が入ることがあるので取り除く
-    if(resArray[resArray.length - 1].length == 0){
-      resArray.pop();
-    }
+  }
+  // 配列の最後に空の要素が入ることがあるので取り除く
+  if(resArray[resArray.length - 1].length == 0){
+    resArray.pop();
   }
 
   // レス指定なしの場合最後の1件取得
@@ -152,12 +165,52 @@ function purseNewResponse(res ,resNum){
   for(; num < resArray.length; num++){
       //パースメソッド呼び出し
     if(resArray[num].length > 0 ){
-      result.push(purseResponse(resArray[num], num));
+      result.push(purseResponse(resArray[num], num + 1));
     }
   }
+  lastResNumber = num + 1;
   // パースしたオブジェクトの配列を返却
   return result;
 }
+
+/**
+*取得したレスポンス（複数）のパース
+*戻りとしてパースしたjsonオブジェクトの配列を返す
+* @param string // res 板から返却されたdat1行分
+* @param string // resNum リクエストされたレス番号
+*/
+function purseDiffResponse(res ,resNum){
+  //結果を格納する配列
+  var result = new Array();
+  // レス番号
+  var num = resNum;
+
+  //新着レスを改行ごとにSplitする
+  var resArray = res.split(/\r\n|\r|\n/);
+  // 新着なしなら戻る。
+  if(resArray.length == 0){
+    return result;
+  } else {
+    // 配列の最後に空の要素が入ることがあるので取り除く
+    if(resArray[resArray.length - 1].length == 0){
+      resArray.pop();
+    }
+  }
+
+  console.log('[Read5ch.purseDiffResponse]取得レス番号=' + num);
+  //1行ごとにパースする
+  resArray.forEach(function(value){
+      //パースメソッド呼び出し
+    if(value.length > 0 ){
+      result.push(purseResponse(value, num));
+      num++;
+    }
+  });
+
+  // パースしたオブジェクトの配列を返却
+  return result;
+}
+
 
 /**レスポンスのパース
 *Jsonオブジェクトを返却する
@@ -190,7 +243,7 @@ function purseResponse(res, num){
   var id = dateId.length == 2 ? dateId[1] : null;
 
   var resJson = {
-    number: num + 1
+    number: parseInt(num)
     ,name: splitRes[0]
     ,email: splitRes[1]
     ,date: date
